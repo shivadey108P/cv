c07ec4fb3a3db24a5206f58772925f923d7122c2
Shiva Dey <105166536+shivadey108P@users.noreply.github.com>
2025-04-12 16:49:21 +0530

    improvements in website

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,42 @@
 document.getElementById('downloadBtn').addEventListener('click', function () {
-    window.open('assets/resume-shiva-dey.docx', '_blank');
+    window.open('https://github.com/shivadey108P/cv/raw/refs/heads/main/assets/resume-shiva-dey.docx', '_blank');
 });
+
+
+document.querySelector('.dropdown').addEventListener('mouseenter', function () {
+    document.querySelector('.dropdown-content').style.display = 'block';
+});
+
+document.querySelector('.dropdown').addEventListener('mouseleave', function () {
+    document.querySelector('.dropdown-content').style.display = 'none';
+});
+
+["summary", "skill", "responsibility", "project", "certificate", "award"].forEach(id => {
+    document.getElementById(id).addEventListener("click", function () {
+        const icon = this.querySelector(".material-symbols-outlined");
+        const content = document.querySelector(`.${id}-content`);
+        const isOpen = content.classList.contains("open");
+
+        if (isOpen) {
+            // collapse it
+            content.style.maxHeight = null;
+            content.classList.remove("open");
+            icon.classList.remove("open");
+        } else {
+            // expand it
+            content.style.maxHeight = content.scrollHeight + "px";
+            content.classList.add("open");
+            icon.classList.add("open");
+        }
+    });
+});
+
+document.getElementById('year').textContent = new Date().getFullYear();
+
+
+document.getElementById('hamburger-menu').addEventListener('click', function () {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('open');
+});
+
+
